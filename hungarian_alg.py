d1c056b97893e25bb75c82abd22b13a0beb38cd9
Marcin Brzózka <mbrzozka@student.agh.edu.pl>
2022-05-02 20:51:00 +0200

    full

--- hungarian_alg.py
+++ hungarian_alg.py
@@ -5,10 +5,10 @@ class Hungarian:
     def __init__(self, matrix):
         self.matrix = matrix  # matrix[task][machine] = cost
         self.theta = 0  # total matrix reduction
-        self.ind_zeros = []  # independent zeros (starring)
-        self.dep_zeros = []  # dependent zeros  (primed)
-        self.cross_row = []  # crossed row
-        self.cross_col = []  # crossed column
+        self.ind_zeros = []  # współrzędne zer niezależnych
+        self.dep_zeros = []  # współrzędne zer zależnych
+        self.cross_row = []  # wiersze do przekreślenia
+        self.cross_col = []  # kolumny do przekreślenia
 
     def reduce_matrix(self):  # method subtracting lowest values from rows and columns
         # rows:
@@ -29,83 +29,83 @@ class Hungarian:
                 self.matrix[i][j] -= min_col_val
 
     def assign(self):
-        ind_zeros = []
-        dep_zeros = []
-        size = len(self.matrix)
-        # Pierwsza iteracja w celu znalezienia pojedynczych zer niezależnych w wierszu
-        for idx, row in enumerate(self.matrix):
-            if row.count(0) == 1:
-                for idy, col in enumerate(row):
-                    if col == 0 and idy not in [el[1] for el in ind_zeros]:
-                        ind_zeros.append((idx, idy))
+        ind_zeros = []  # współrzędne zer niezależnych
+        dep_zeros = []  # współrzędne zer zależnych
+        size = len(self.matrix)  # rozmiar macierzy
 
-        # Kolejna iteracja w celu znalezienia zera niezależnego w wierszach z większą ilością zer
-        for number in range(2, size):
-            for idx, row in enumerate(self.matrix):
-                if row.count(0) == number:
-                    added = False
-                    for idy, col in enumerate(row):
+        for number in range(1, size):  # Dla liczby zer w wierszu
+            for idx, row in enumerate(self.matrix):  # dla każdego wiersza macierzy
+                if row.count(0) == number:  # jeśli liczba zer w wierszu wynosi number
+                    added = False  # zmienna do sprawdzania czy znaleziono zero niezalezne
+                    for idy, col in enumerate(row):  # dla każdej kolumny
+                        # jeśli znaleziono zero, nie jest to zero niezależne i kolumna nie jest już w innym zerze
                         if col == 0 and not added and idy not in [el[1] for el in ind_zeros]:
-                            another_zero_in_col = False
-                            for row_in_col in range(size):
+                            another_zero_in_col = False  # zmiienna do spradzania większej ilości zer w kolumnie
+                            for row_in_col in range(size):  # dla każdego wiersza w kolumnie
+                                # jeśli znaleziono zero i nie jest to wiersz row
                                 if self.matrix[row_in_col][idy] == 0 and row_in_col != row:
-                                    another_zero_in_col = True
-                            if not another_zero_in_col:
-                                added = True
-                                ind_zeros.append((idx, idy))
+                                    another_zero_in_col = True  # znaleziono zero
+                            if not another_zero_in_col:  # jeśli nie znaleziono więcej zer
+                                added = True  # znaleziono zero niezalezne
+                                ind_zeros.append((idx, idy))  # dodaj współrzędną do zer niezależnych
 
-                    if not added:
-                        for idy, col in enumerate(row):
+                    if not added:  # jeśli nie znaleziono zera niezależnego
+                        for idy, col in enumerate(row):  # dla każdej kolumny macierzy
+                            #  znajdź pierwsze zero, którego kolumna nie jest już w zerach niezaleznych
                             if col == 0 and idy not in [el[1] for el in ind_zeros]:
-                                ind_zeros.append((idx, idy))
+                                ind_zeros.append((idx, idy))  # dodaj do zer niezależnych
                                 break
 
-            for row in range(size):
-                for col in range(size):
+            # dodanie zer zależnych
+            for row in range(size):  # dla każdego wiersza
+                for col in range(size):  # dla każdej kolumny
+                    # jeśli znaleziono zero i nie jest to zero niezależne
                     if self.matrix[row][col] == 0 and (row, col) not in ind_zeros:
-                        dep_zeros.append((row, col))
+                        dep_zeros.append((row, col))  # znaleziono zero zależne
 
         self.ind_zeros = ind_zeros
         self.dep_zeros = dep_zeros
 
     def zero_crossing(self):
-        select_row = [i for i in range(len(self.matrix))]
-        select_col = []
+        select_row = [i for i in range(len(self.matrix))]  # tablica na zaznaczone wiersze
+        select_col = []  # tablica na zaznaczone kolumny
         # Oznaczyć symbolem x każdy wiersz nie posiadający niezależnego zera
-        for el in self.ind_zeros:
-            select_row.remove(el[0])
+        for el in self.ind_zeros:  # dla każdego elementu w tablicy zer niezależnych
+            select_row.remove(el[0])  # usuń wiersz z tablicy zaznaczonych, w którym jest zero niezależne
 
-        while True:
-            selected = False
+        while True:  # do póki znajdowane są nowe zaznaczenia
+            selected = False  # zmienna do sprawdzania czy znaleziono nowe zaznaczenia
+
             # Oznaczyć symbolem x każda kolumnę mającą zero zależne 0 w oznaczonym wierszu
-            for row in select_row:
-                # for col in matrix[row]:
-                for col in range(len(self.matrix[row])):
-                    for zero in self.dep_zeros:
-                        if zero[0] == row and zero[1] == col:
-                            if col not in select_col:
-                                select_col.append(col)
-                                selected = True
+            for row in select_row:  # dla każdego wiersza w zaznaczonych wierszach
+                for col in range(len(self.matrix[row])):  # dla każdej kolumny macierzy
+                    # for zero in self.dep_zeros:  # dla każdego zera zależnego
+                    #     if zero[0] == row and zero[1] == col:
+                    if (row, col) in self.dep_zeros:  # jeśli współrzędne są równe współrzędnym zera zależnego
+                        if col not in select_col:  # jeśli kolumna nie jest w zaznaczonych kolumnach
+                            select_col.append(col)  # dodaj do zaznaczonych kolumn
+                            selected = True  # zaznaczono
 
             # Oznaczyć symbolem x każdy wiersz mający w oznakowanej kolumnie niezależne zero
-            for col in select_col:
-                for row in range(len(self.matrix)):
-                    for zero in self.ind_zeros:
-                        if zero[0] == row and zero[1] == col:
-                            if row not in select_row:
-                                select_row.append(row)
-                                selected = True
-            if not selected:
-                break
+            for col in select_col:  # dla każdej kolumny w zaznaczonych kolumnach
+                for row in range(len(self.matrix)):  # dla każdego wiersza macierzy
+                    # for zero in self.ind_zeros:
+                    #     if zero[0] == row and zero[1] == col:
+                    if (row, col) in self.ind_zeros:  # jeśli współrzędne są równe współrzędnym zera niezależnego
+                        if row not in select_row:  # jeśli wiersz nie jest w zaznaczonych wierszach
+                            select_row.append(row)  # dodaj do zaznaczonych wierszy
+                            selected = True  # zaznaczono
+            if not selected:  # jeśli nie zaznaczono
+                break  # przerwij
 
         # Poszukiwanie minimalnego pokrycia wierzchołkowego.
-        cross_col = select_col
-        cross_col.sort()
-        cross_row = []
-        for i in range(len(self.matrix)):
-            if i not in select_row:
-                cross_row.append(i)
-        cross_row.sort()
+        cross_col = select_col  # tablica kolumn do skreślenia jest równa tablicy zaznaczonych kolumn
+        cross_col.sort()  # posortuj tablice kolumn
+        cross_row = []  # tablica kolumn do skreślenia
+        for i in range(len(self.matrix)):  # dla każdego wiersza macierzy
+            if i not in select_row:  # jeśli wiersz nie jest zaznaczony
+                cross_row.append(i)  # dodaj wiersz do skreślenia
+        cross_row.sort()  # posortuj tablice wierszy
         self.cross_row = cross_row
         self.cross_col = cross_col
 
@@ -189,7 +189,7 @@ matrix_e = [
     [4, 1, 2, 4, 0]
 ]
 
-matrix_2 = [
+matrix = [
     [3, 2, 4, 5, 2, 1],
     [7, 3, 4, 5, 1, 1],
     [2, 2, 3, 3, 5, 6],
@@ -198,5 +198,5 @@ matrix_2 = [
     [1, 0, 2, 4, 2, 3]
 ]
 
-m = Hungarian(matrix_2)
+m = Hungarian(matrix)
 m.algorithm()
