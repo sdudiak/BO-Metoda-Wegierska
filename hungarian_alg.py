cb50804a1cc6b374fb2a548a14d45f7f9324965d
sdudiak <staszek.dudiak@irpark.pl>
2022-05-02 11:49:22 +0200

    minor fixes

--- hungarian_alg.py
+++ hungarian_alg.py
@@ -10,7 +10,7 @@ class Hungarian():
         # rows:
         for i,_ in enumerate(self.matrix):
             self.theta += min(self.matrix[i])
-            self.matrix[i] = [elem - min(self.matrix[i]) for elem in matrix[i]]
+            self.matrix[i] = [elem - min(self.matrix[i]) for elem in self.matrix[i]]
         # cols:
         for j,_ in enumerate(self.matrix):
             min_col_val = inf
@@ -21,9 +21,34 @@ class Hungarian():
 
             for i,_ in enumerate(self.matrix[i]): # subtract minimal value
                 self.matrix[i][j] -= min_col_val
-        
             
+    def get_more_independent_zeros(self): # method for further matrix reduction
+        min_matrix_val = inf
+        crossed_rows = [] # temporary arrays, while proper ones are being developed
+        crossed_cols = []
 
+        for i,_ in enumerate(self.matrix): # find minimal value uncrossed value in matrix
+            for j in enumerate(self.matrix[i]):
+                if i not in crossed_rows and j not in crossed_cols and self.matrix[i][j] < min_matrix_val:
+                    min_matrix_val = self.matrix[i][j]
+        # add minval
+        multiplicator = 1 # how many times multiply min value
+        for i,_  in enumerate(self.matrix): # subtract minimal value from uncrossed elems
+            if i in crossed_rows : continue
+            for j,_ in enumerate(self.matrix[i]):
+                if i in crossed_rows and j in crossed_cols: 
+                    self.matrix[i][j] += min_matrix_val # add minimal value to double crossed elements
+                    multiplicator -= 1
+                if j in crossed_cols : continue
+                self.matrix[i][j] -= min_matrix_val
+                multiplicator += 1
+        self.theta += multiplicator * min_matrix_val
+
+        
+        
+
+
+        
 
 
 
